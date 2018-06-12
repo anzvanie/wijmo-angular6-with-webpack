c70a67f5136f5b747957751f201578827a696858
anzvanie <anzvanie1510@gmail.com>
2018-06-12 13:25:55 +0700

    Bruce (Van) Nguyen add source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { WjFlexGrid } from 'wijmo/wijmo.angular2.grid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('grid') grid: WjFlexGrid;
+
+  data: any[];
+
+  ngOnInit(): void {
+    const people = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S'.split(',');
+    const minAge = 23, maxAge = 35;
+    this.data = [];
+
+    for (let i = 0; i < people.length; i++) {
+      this.data.push({
+        person: people[i],
+        age: Math.floor(Math.random() * (maxAge - minAge + 1)) + minAge,
+        salary: Math.floor(Math.random() * 1000),
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { WjGridModule } from 'wijmo/wijmo.angular2.grid';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WjGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
